17564a546296461910203626559129a964266e56
miss-invincible <shivangi.gupta.jiit@gmail.com>
2018-11-12 18:42:38 +0530

    created purple tentable plug

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,13 @@
 # name: basic-plugin
 # about: A super simple plugin to demonstrate how plugins work
 # version: 0.0.1
-# authors: Awesome Plugin Developer
+# authors: Shivangi
+# url : https://github.com/miss-invincible/discourse_basic_plugin
 enabled_site_setting :awesomeness_enabled
+
+add_admin_route 'purple_tentacle.title', 'purple-tentacle'
+
+Discourse::Application.routes.append do
+  get '/admin/plugins/purple-tentacle' => 'admin/plugins#index', constraints: StaffConstraint.new
+end
+
